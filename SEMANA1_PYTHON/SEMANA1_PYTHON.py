c40a9fa7d55028ab4dfc8e2c183c170d8059e290
jesus <jesusprincipecastro@gmail.com>
2025-09-02 15:30:40 -0500

    subiendo ejersicio1-SEM1

---
+++ SEMANA1_PYTHON/SEMANA1_PYTHON.py
@@ -0,0 +1,5 @@
+def ejer1(): #creando metodo ejer1
+    nombre = input("ingrese su nombre: ")
+    carrera = input ("ingrese su carrera: ")
+
+    print(f"\n{nombre}, bienvenido a FA de {carrera}") 
---
+++ semana1_c#/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana1_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+
+        }
+        static void ejer1()
+        {
+            string nombre, carrera; //declarar variables
+
+            Console.Write("ingrese su nombre");
+            nombre = Console.ReadLine();
+
+            Console.Write("ingrese su carrera");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido a FA de {carrera}");
+        }
+
+
+
+
+
+
+
+        static void ejer2()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+        static void ejer3()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+        static void ejer4()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+        static void ejer5()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+        static void ejer6()
+        {
+
+        }
+    }
+}
