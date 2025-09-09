3e19ece93680b86f0b71729f028135607ff01cb3
jesus <ramosbozajoseanthony967@gmail.com>
2025-09-09 15:57:28 -0500

    ejer2 sem3

--- semana4_PY/semana4_PY.py
+++ semana4_PY/semana4_PY.py
@@ -1,1 +1,10 @@
+def ejer1():
+    edad = int(input("Ingrese una edad: "))
 
+    if edad < 18:
+        print("Menor de edad")
+    else:
+        if edad >= 18 and edad <=64:
+            print("Adulto")
+        else:
+            print("adulto mayor")
